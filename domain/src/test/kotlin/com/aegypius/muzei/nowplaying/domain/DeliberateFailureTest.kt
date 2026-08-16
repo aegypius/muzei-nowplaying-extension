ee4d3e8af188a9568cb1eb34262bfb3226e728af
aegypius <git@aegypius.com>
2026-08-16 08:29:26 +0200

    test: prove CI can fail

---
+++ domain/src/test/kotlin/com/aegypius/muzei/nowplaying/domain/DeliberateFailureTest.kt
@@ -0,0 +1,10 @@
+package com.aegypius.muzei.nowplaying.domain
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+/** Deleted with this branch. Exists to prove CI can go red. */
+class DeliberateFailureTest {
+    @Test
+    fun `this test fails on purpose`() = assertEquals("green", "red")
+}
